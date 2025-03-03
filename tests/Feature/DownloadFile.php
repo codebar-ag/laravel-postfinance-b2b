bbc192cb30d3ca01e3dc3bce80670a2c0ae8166c
Sebastian Fix <sebastian.fix@codebar.ch>
2025-03-03 17:38:42 +0700

    WIP

--- tests/Feature/DownloadFile.php
+++ tests/Feature/DownloadFile.php
@@ -10,11 +10,11 @@ it('b2b service downloadFile test', function () {
     $config = config('postfinance-b2b');
 
     $payerId = config('postfinance-b2b.tests.payer_id');
-    $billerId = '41101000000834484'; // config('postfinance-b2b.tests.biller_id');
-    $transactionId = '9360415238'; // config('postfinance-b2b.tests.transaction_id');
+    $billerId = config('postfinance-b2b.tests.biller_id');
+    $transactionId = config('postfinance-b2b.tests.transaction_id');
     $fileType = config('postfinance-b2b.tests.file_type');
 
     $client = PostfinanceClientFactory::factory($config);
-    $response = $client->downloadFile(new GetInvoicePayer($payerId, $billerId, $transactionId, $fileType));
+    $client->downloadFile(new GetInvoicePayer($payerId, $billerId, $transactionId, $fileType));
 
 })->group('get', 'test')->only();
